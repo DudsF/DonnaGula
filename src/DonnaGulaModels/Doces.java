52e3d74aa21f2ebdda49d766a48ef4dc62134a33
20151074010611 <20151074010611@MC266466.ifrn.local>
2019-01-07 11:21:13 -0300

    IAI

---
+++ src/DonnaGulaModels/Doces.java
@@ -0,0 +1,32 @@
+package DonnaGulaModels;
+
+public class Doces {
+	private long id;
+	private String nome;
+	private String recheio;
+	private int preco;
+	
+	@Override
+	public String toString() {
+		return "doces [id=" + id + ", nome=" + nome + ", recheio=" + recheio + ", preco=" + preco + "]";
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getRecheio() {
+		return recheio;
+	}
+	public void setRecheio(String recheio) {
+		this.recheio = recheio;
+	}
+	public int getPreco() {
+		return preco;
+	}
+	public void setPreco(int preco) {
+		this.preco = preco;
+	}
+	
+}
